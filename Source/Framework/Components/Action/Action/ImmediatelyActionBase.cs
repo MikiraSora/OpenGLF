709435389854244ff0352264abaca52dd7b0aeec
MikiraSora <mikirasora0409@126.com>
2017-02-06 21:02:00 +0800

    recommit

--- Source/Framework/Components/Action/Action/ImmediatelyActionBase.cs
+++ Source/Framework/Components/Action/Action/ImmediatelyActionBase.cs
@@ -3,9 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace OpenGLF.System
+namespace OpenGLF
 {
-    class ImmediatelyAction
+    class ImmediatelyActionBase : ActionBase
     {
+        public ImmediatelyActionBase(GameObject gameobject) : base(0, 0, null, gameobject) {}
+
+        public override void onAction(float passTime)
+        {
+            onUpdate(0); 
+        }
+
+        public override void onStart()
+        {
+            
+        }
+
+        public override float timeCast()
+        {
+            return 0;
+        }
+
+        public override void onFinish()
+        {
+            
+        }
+
+        public override ActionBase reverse()
+        {
+            return this; //Normally,immediately action can reuse without any resetting operation.
+        }
+
+        public override void onUpdate(float norValue)
+        {
+            onUpdate();
+            markDone(); // just run once
+        }
+
+        public virtual void onUpdate()
+        {
+
+        }
     }
 }
